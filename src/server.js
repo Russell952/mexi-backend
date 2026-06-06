657ec2274a8e81cc2771e3c7b8fccd147002c2b1
Russell Eshiet <russelleshiet107@gmail.com>
2026-06-06 12:50:49 +0100

    added new origin to cors

--- src/server.js
+++ src/server.js
@@ -16,6 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:4173",
       "https://mexi-medicals.vercel.app/",
       "https://your-admin.vercel.app"
     ],
