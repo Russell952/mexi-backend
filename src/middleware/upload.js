1cad682831887424f30fe84e2a568f28cb3822e6
Russell Eshiet <russelleshiet107@gmail.com>
2026-06-22 00:09:07 +0100

    cloudinary issues

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,23 +1,16 @@
 const multer = require("multer");
 
-const CloudinaryStorage = require("multer-storage-cloudinary");
-
-console.log(
-  require("multer-storage-cloudinary")
-);
-
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary,
-
   params: {
     folder: "Mexi Medicals",
-
     allowed_formats: ["jpg", "png", "jpeg"],
   },
 });
 
 const upload = multer({ storage });
 
-module.exports = upload;
+module.exports = upload; 
